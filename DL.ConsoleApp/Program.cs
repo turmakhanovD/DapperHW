649b0723b1dc4c3b03e68fbbeb58de73a5eaec8e
Турмахан Динислам Даулетулы <@corp.itstep>
2019-04-23 17:48:21 +0600

    third

---
+++ DL.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using DL.DataAccess;
+using DL.Models;
+using System;
+
+namespace DL.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var repository = new ReceiversRepository())
+            {
+             
+
+
+             
+              
+            }
+        }
+    }
+}
